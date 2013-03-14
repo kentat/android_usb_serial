4b4f263e63f069bae426ad0f62356c4f7b6f4bdb
kentaro takahashi <ktaka@betterworld.jp>
2013-03-15 02:05:34 +0900

    ui modified

--- Sample02/gen/com/example/sample02/R.java
+++ Sample02/gen/com/example/sample02/R.java
@@ -22,6 +22,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int shape_button_corners=0x7f020001;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f0a0000;
--- Sample02/src/com/example/sample02/MainActivity.java
+++ Sample02/src/com/example/sample02/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 import android.widget.EditText;
 import android.util.Log;
 import android.content.Context;
@@ -36,6 +37,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 
 		mHandler = new Handler();
@@ -52,8 +54,8 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				LogOut("Button was clicked.[OK]");
-				mTextSnd.append( mEditCmd.getText().toString() );
-				mTextRcv.append( mEditCmd.getText().toString() );
+				mTextSnd.append( mEditCmd.getText().toString()+"\n" );
+				mTextRcv.append( mEditCmd.getText().toString()+"\n" );
 
 				//try{
 					//mSerialDevice.write(mEditCmd.toString().getBytes("UTF-8"),
